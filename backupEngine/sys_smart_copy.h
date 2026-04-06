e0e12e8e032c0c4198cb2f7277955d29f5a5e99f
malaura_t <mltafurg@eafit.edu.co>
2026-04-06 17:11:42 -0500

    Refine comments in sys_smart_copy.h
    Updated comments for clarity and accuracy.

--- backupEngine/sys_smart_copy.h
+++ backupEngine/sys_smart_copy.h
@@ -1,12 +1,14 @@
 #ifndef SYS_SMART_COPY_H
 #define SYS_SMART_COPY_H
-
+// Tamaño de página estándar (4KB). 
+// Optimiza el movimiento de datos al alinearse con los bloques del Kernel.
 #define BUFFER_SIZE 4096
 
-// Función núcleo: Copia binaria 
+// Función: realiza una copia de bit a bit 
 int sys_smart_copy(const char *src, const char *dest);
 
-// Función de monitoreo: Lógica del demonio para archivos
+// Función de monitoreo: Lógica del demonio para archivos, 
+//es decir es aquella que implementamos para que el proceso se realice en segundo plano
 void iniciar_demonio_archivo(const char *src, const char *dest, int segundos);
 
 #endif
